4da3db56ed859bc405d18c64a49458cfd637f1c2
Wessel Stoop <woseseltops@hotmail.com>
2025-04-23 09:48:51 +0000

    Initial suggestion by copilot for listview List view #3

--- PiN/PiN/urls.py
+++ PiN/PiN/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from papyrus.views import PapyrusSideListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('papyrus-sides/', PapyrusSideListView.as_view(), name='papyrus_side_list'),
 ]
