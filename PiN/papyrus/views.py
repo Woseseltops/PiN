076a4dececf9cafae7ad88399bfdbd19406c2f82
Wessel Stoop <woseseltops@hotmail.com>
2025-07-04 11:54:41 +0200

    Added first version of detail view #2

--- PiN/papyrus/views.py
+++ PiN/papyrus/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import PapyrusSide
+from .models import PapyrusSide, Papyrus
 
 class PapyrusSideListView(ListView):
     model = PapyrusSide
     template_name = 'papyrus/papyrus_side_list.html'
     context_object_name = 'papyrus_sides'
 
-# Create your views here.
+class PapyrusDetailView(ListView):
+    model = Papyrus
+    template_name = 'papyrus/papyrus_detail.html'
+    context_object_name = 'papyrus'
+
+    def get_queryset(self):
+        return Papyrus.objects.filter(id=self.kwargs['pk'])[0]
