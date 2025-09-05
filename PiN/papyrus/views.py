b480ffe642f72b240165194e29bc75934b241c23
Wessel Stoop <woseseltops@hotmail.com>
2025-09-05 13:15:12 +0200

    Added basic page and navigation bar #4

--- PiN/papyrus/views.py
+++ PiN/papyrus/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import PapyrusSide, Papyrus
+from django.http import Http404
+from .models import PapyrusSide, Papyrus, Page
 
 class PapyrusSideListView(ListView):
     model = PapyrusSide
     template_name = 'papyrus/papyrus_side_list.html'
     context_object_name = 'papyrus_sides'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu_pages'] = Page.objects.order_by('menu_bar_order')
+        return context
+
 class PapyrusDetailView(ListView):
     model = Papyrus
     template_name = 'papyrus/papyrus_detail.html'
     context_object_name = 'papyrus'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu_pages'] = Page.objects.order_by('menu_bar_order')
+        return context
+
     def get_queryset(self):
         return Papyrus.objects.filter(id=self.kwargs['pk'])[0]
+
+def page_view(request, page_name):
+    from .models import Page
+    try:
+        page = Page.objects.get(name=page_name)
+    except Page.DoesNotExist:
+        raise Http404("Page not found")
+    menu_pages = Page.objects.order_by('menu_bar_order')
+    return render(request, 'papyrus/page.html', {'page': page, 'menu_pages': menu_pages})
+
+def home_page_view(request):
+    from .models import Page
+    try:
+        page = Page.objects.get(is_home=True)
+    except Page.DoesNotExist:
+        raise Http404("Home page not found")
+    menu_pages = Page.objects.order_by('menu_bar_order')
+    return render(request, 'papyrus/page.html', {'page': page, 'menu_pages': menu_pages})
